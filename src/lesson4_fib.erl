9b957ad38b3e092a7ecc14900f9c4aa5ca7de2ce
7006 <22399553+7006@users.noreply.github.com>
2023-09-07 14:14:48 +0000

    Add tail recursion version

--- src/lesson4_fib.erl
+++ src/lesson4_fib.erl
@@ -1,6 +1,6 @@
 -module(lesson4_fib).
 
--export([fib/1]).
+-export([fib/1, fib_tail/1]).
 
 fib(0) ->
     1;
@@ -8,3 +8,11 @@ fib(1) ->
     1;
 fib(N) when N > 1 ->
     fib(N - 1) + fib(N - 2).
+
+fib_tail(N) when N >= 0 ->
+    fib_tail(N, -1, 0, 1).
+
+fib_tail(N, N, X, _) ->
+    X;
+fib_tail(N, I, X, Y) ->
+    fib_tail(N, I + 1, X + Y, X).
--- test/lesson4_fib_tests.erl
+++ test/lesson4_fib_tests.erl
@@ -2,13 +2,6 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--define(_assertWithinTimespan(Expr, N, TimeUnit), begin
-    Start = erlang:system_time(TimeUnit),
-    Expr,
-    End = erlang:system_time(TimeUnit),
-    ?_assert(End - Start =< 1)
-end).
-
 fib_test_() ->
     [
         ?_assert(lesson4_fib:fib(0) =:= 1),
@@ -18,6 +11,17 @@ fib_test_() ->
         ?_assert(lesson4_fib:fib(4) =:= 5),
         ?_assert(lesson4_fib:fib(5) =:= 8),
         ?_assertException(error, function_clause, lesson4_fib:fib(-1)),
-        ?_assert(lesson4_fib:fib(31) =:= 2178309),
-        ?_assertWithinTimespan(lesson4_fib:fib(31), 1, second)
+        ?_assert(lesson4_fib:fib(31) =:= 2178309)
+    ].
+
+fib_tail_test_() ->
+    [
+        ?_assert(lesson4_fib:fib_tail(0) =:= 1),
+        ?_assert(lesson4_fib:fib_tail(1) =:= 1),
+        ?_assert(lesson4_fib:fib_tail(2) =:= 2),
+        ?_assert(lesson4_fib:fib_tail(3) =:= 3),
+        ?_assert(lesson4_fib:fib_tail(4) =:= 5),
+        ?_assert(lesson4_fib:fib_tail(5) =:= 8),
+        ?_assertException(error, function_clause, lesson4_fib:fib_tail(-1)),
+        ?_assert(lesson4_fib:fib_tail(31) =:= 2178309)
     ].
