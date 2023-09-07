9f0b1b662146d0792a0583c8ec3162576c05d704
7006 <22399553+7006@users.noreply.github.com>
2023-09-07 15:19:47 +0000

    Add performance measure tests

--- src/lesson4_fib.erl
+++ src/lesson4_fib.erl
@@ -9,10 +9,11 @@ fib(1) ->
 fib(N) when N > 1 ->
     fib(N - 1) + fib(N - 2).
 
-fib_tail(N) when N >= 0 ->
-    fib_tail(N, -1, 0, 1).
 
-fib_tail(N, N, X, _) ->
-    X;
-fib_tail(N, I, X, Y) ->
-    fib_tail(N, I + 1, X + Y, X).
+fib_tail(N) ->
+    fib_tail(N, 0, 1).
+
+fib_tail(0, X, Y) ->
+    X + Y;
+fib_tail(N, X, Y) when N > 0 ->
+    fib_tail(N - 1, X + Y, X).
--- test/lesson4_fib_tests.erl
+++ test/lesson4_fib_tests.erl
@@ -2,6 +2,13 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+-define(elapsed_time(Expr), begin
+    Start = erlang:system_time(second),
+    Expr,
+    End = erlang:system_time(second),
+    End - Start
+end).
+
 fib_test_() ->
     [
         ?_assert(lesson4_fib:fib(0) =:= 1),
@@ -24,4 +31,34 @@ fib_tail_test_() ->
         ?_assert(lesson4_fib:fib_tail(5) =:= 8),
         ?_assertException(error, function_clause, lesson4_fib:fib_tail(-1)),
         ?_assert(lesson4_fib:fib_tail(31) =:= 2178309)
+    ].
+
+perf_test_() ->
+    [
+        {
+            "fib(40) completes > 1 second",
+            timeout,
+            10,
+            ?_assert(?elapsed_time(lesson4_fib:fib(40)) >= 1)
+        },
+        {
+            "fib_tail(40) completes < 1 second",
+            ?_assert(?elapsed_time(lesson4_fib:fib_tail(40)) =< 1)
+        },
+        {
+            "fib_tail(50) completes < 1 second",
+            ?_assert(?elapsed_time(lesson4_fib:fib_tail(50)) =< 1)
+        },
+        {
+            "fib_tail(100) completes < 1 second",
+            ?_assert(?elapsed_time(lesson4_fib:fib_tail(100)) =< 1)
+        },
+        {
+            "fib_tail(1_000) completes < 1 second",
+            ?_assert(?elapsed_time(lesson4_fib:fib_tail(1_000)) =< 1)
+        },
+        {
+            "fib_tail(10_000) completes < 1 second",
+            ?_assert(?elapsed_time(lesson4_fib:fib_tail(10_000)) =< 1)
+        }
     ].
